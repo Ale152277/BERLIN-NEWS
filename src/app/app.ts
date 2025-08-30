0954998b52666232835d3a06acfdda86600ff2ac
Ale152277 <gsjoaquin22@gmail.com>
2025-08-30 13:05:19 -0500

    Agregué footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { NewsList } from './components/news-list/news-list';
 import { Header } from './components/header/header';
 import { NewsColumn } from './components/news-column/news-column';
+import { Footer } from "./components/footer/footer";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [NewsList,NewsColumn, Header],
+  imports: [NewsList, NewsColumn, Header, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
