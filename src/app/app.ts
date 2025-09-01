3b13a0dbc72d6acad1e0f6b61db58b40e583923f
YuleisyQuipuzcoa22 <yuleisyquipuzcoa@gmail.com>
2025-09-01 18:52:36 -0500

    solo borre el router oulet porque no usamos rutas jeje

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,10 @@ import { NewsList } from './components/news-list/news-list';
 import { Header } from './components/header/header';
 import { NewsColumn } from './components/news-column/news-column';
 import { Footer } from "./components/footer/footer";
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [NewsList, NewsColumn, Header, Footer, RouterOutlet],
+  imports: [NewsList, NewsColumn, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
